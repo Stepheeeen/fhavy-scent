177ab0f9b692575d7f209145640f05dea1bd33b2
Stepheeeen <Stephenon56@gmail.com>
2025-04-27 00:14:34 +0100

    Update HTML files for Fhavy-Scent branding
    - Changed all instances of "Stelina" to "Fhavy-Scent" in checkout.html, contact.html, gridproducts.html, index.html, and shoppingcart.html.
    - Updated meta titles and comments to reflect the new branding.
    - Adjusted class names and data attributes to maintain consistency with the new branding.

--- assets/js/frontend-plugin.js
+++ assets/js/frontend-plugin.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function ($) {
     "use strict";
-    function stelina_init_menu_toggle() {
-        var contain = '.stelina-nav-toggle';
+    function Fhavy-Scent_init_menu_toggle() {
+        var contain = '.Fhavy-Scent-nav-toggle';
         $(contain).each(function () {
             var _main = $(this);
             _main.children('.menu-item.parent').each(function () {
@@ -51,15 +51,15 @@ jQuery(document).ready(function ($) {
         $(this).closest('.text-404').find('.search-form').toggleClass('open');
         return false;
     });
-    // ----------stelina_custom_scrollbar-------------------
-    function stelina_custom_scrollbar() {
-        $('.stelina-mini-cart .minicart-items').mCustomScrollbar();
-        $('.stelina-mini-cart .minicart-items').change(function () {
-            $('.stelina-mini-cart .minicart-items').mCustomScrollbar();
+    // ----------Fhavy-Scent_custom_scrollbar-------------------
+    function Fhavy-Scent_custom_scrollbar() {
+        $('.Fhavy-Scent-mini-cart .minicart-items').mCustomScrollbar();
+        $('.Fhavy-Scent-mini-cart .minicart-items').change(function () {
+            $('.Fhavy-Scent-mini-cart .minicart-items').mCustomScrollbar();
         });
     }
 
-    function stelina_custom_scrollbar_header_nav() {
+    function Fhavy-Scent_custom_scrollbar_header_nav() {
         $('.header.vertical-style .header-nav .container-wapper').mCustomScrollbar();
         $('.header.vertical-style .header-nav .container-wapper').change(function () {
             $('.header.vertical-style .header-nav .container-wapper').mCustomScrollbar();
@@ -67,12 +67,12 @@ jQuery(document).ready(function ($) {
     }
 
     //------------------ Video Lightbox------------
-    function stelina_video() {
+    function Fhavy-Scent_video() {
         $('.quick-install').simpleLightboxVideo();
     }
 
     // --------------------remove_class_equal--------------------------
-    function stelina_remove_class_review() {
+    function Fhavy-Scent_remove_class_review() {
         var _winw = $(window).innerWidth();
         if (_winw < 992) {
             $('.sevice-item.style-1').removeClass('equal-container').find('.equal-element').removeAttr('style');
@@ -82,8 +82,8 @@ jQuery(document).ready(function ($) {
         }
     }
 
-    // -----------stelina_details_thumd--------------------
-    function stelina_details_thumd_zoom() {
+    // -----------Fhavy-Scent_details_thumd--------------------
+    function Fhavy-Scent_details_thumd_zoom() {
         /* ------------------------------------------------
          Arctic modal
          ------------------------------------------------ */
@@ -181,7 +181,7 @@ jQuery(document).ready(function ($) {
 
     $(".chosen-select").chosen({disable_search_threshold: 10});
     // ====================isotop========================
-    function stelina_masonry() {
+    function Fhavy-Scent_masonry() {
         var masonry = $('.masonry-grid').isotope({
             // set itemSelector so .grid-sizer is not used in layout
             itemSelector: '.grid-item',
@@ -195,30 +195,30 @@ jQuery(document).ready(function ($) {
     }
 
     /* TOGGLE */
-    function stelina_dropdown() {
+    function Fhavy-Scent_dropdown() {
         $(document).on('click', '.header-control .close', function () {
-            $(this).closest('.stelina-dropdown').removeClass('open');
+            $(this).closest('.Fhavy-Scent-dropdown').removeClass('open');
         });
         $(document).on('click', function (event) {
-            var _target = $(event.target).closest('.stelina-dropdown');
-            var _allparent = $('.stelina-dropdown');
+            var _target = $(event.target).closest('.Fhavy-Scent-dropdown');
+            var _allparent = $('.Fhavy-Scent-dropdown');
 
             if (_target.length > 0) {
                 _allparent.not(_target).removeClass('open');
                 if (
-                    $(event.target).is('[data-stelina="stelina-dropdown"]') ||
-                    $(event.target).closest('[data-stelina="stelina-dropdown"]').length > 0
+                    $(event.target).is('[data-Fhavy-Scent="Fhavy-Scent-dropdown"]') ||
+                    $(event.target).closest('[data-Fhavy-Scent="Fhavy-Scent-dropdown"]').length > 0
                 ) {
                     _target.toggleClass('open');
                     return false;
                 }
             } else {
-                $('.stelina-dropdown').removeClass('open');
+                $('.Fhavy-Scent-dropdown').removeClass('open');
             }
         });
     }
 
-    function stelina_mobile_block() {
+    function Fhavy-Scent_mobile_block() {
         $(document).on('click', '.header-device-mobile .item.has-sub>a', function () {
             $(this).closest('.header-device-mobile').find('.item').removeClass('open');
 
@@ -237,7 +237,7 @@ jQuery(document).ready(function ($) {
     }
 
     // =====================slick============================
-    function stelina_init_carousel() {
+    function Fhavy-Scent_init_carousel() {
         $('.owl-slick').not('.slick-initialized').each(function () {
             var _this = $(this),
                 _responsive = _this.data('responsive'),
@@ -279,9 +279,9 @@ jQuery(document).ready(function ($) {
     /* ---------------------------------------------
      TAB EFFECT
      --------------------------------------------- */
-    function stelina_tab_fade_effect() {
+    function Fhavy-Scent_tab_fade_effect() {
         // effect click
-        $(document).on('click', '.stelina-tabs .tab-link a', function () {
+        $(document).on('click', '.Fhavy-Scent-tabs .tab-link a', function () {
             var tab_id = $(this).attr('href');
             var tab_animated = $(this).data('animate');
 
@@ -623,11 +623,11 @@ jQuery(document).ready(function ($) {
     }
 
     // -----------------count down years months -------------------------------
-    function stelina_countdown() {
-        if ($('.stelina-countdown').length > 0) {
+    function Fhavy-Scent_countdown() {
+        if ($('.Fhavy-Scent-countdown').length > 0) {
             var labels = ['Years', 'Months', 'Weeks', 'Days', 'Hrs', 'Mins', 'Secs'];
             var layout = '<span class="box-count day"><span class="number">{dnn}</span> <span class="text">Days</span></span><span class="box-count hrs"><span class="number">{hnn}</span> <span class="text">Hrs</span></span><span class="box-count min"><span class="number">{mnn}</span> <span class="text">Mins</span></span><span class="box-count secs"><span class="number">{snn}</span> <span class="text">Secs</span></span>';
-            $('.stelina-countdown').each(function () {
+            $('.Fhavy-Scent-countdown').each(function () {
                 var austDay = new Date($(this).data('y'), $(this).data('m') - 1, $(this).data('d'), $(this).data('h'), $(this).data('i'), $(this).data('s'));
                 $(this).countdown({
                     until: austDay,
@@ -639,31 +639,31 @@ jQuery(document).ready(function ($) {
     };
     // --------------------------------------------------------
     $(window).scroll(function () {
-        stelina_custom_scrollbar();
+        Fhavy-Scent_custom_scrollbar();
     });
     $(window).resize(function () {
         quickview_popup();
         kt_resizeMegamenu();
-        stelina_remove_class_review();
-        stelina_details_thumd_zoom();
-        stelina_custom_scrollbar();
+        Fhavy-Scent_remove_class_review();
+        Fhavy-Scent_details_thumd_zoom();
+        Fhavy-Scent_custom_scrollbar();
     });
     $(window).load(function () {
         newletter_popup();
         quickview_popup();
-        stelina_mobile_block();
-        stelina_remove_class_review();
-        stelina_custom_scrollbar()
+        Fhavy-Scent_mobile_block();
+        Fhavy-Scent_remove_class_review();
+        Fhavy-Scent_custom_scrollbar()
     });
-    stelina_dropdown();
-    stelina_init_carousel();
-    stelina_remove_class_review();
-    stelina_tab_fade_effect();
-    stelina_details_thumd_zoom();
-    stelina_video();
+    Fhavy-Scent_dropdown();
+    Fhavy-Scent_init_carousel();
+    Fhavy-Scent_remove_class_review();
+    Fhavy-Scent_tab_fade_effect();
+    Fhavy-Scent_details_thumd_zoom();
+    Fhavy-Scent_video();
     kt_resizeMegamenu();
-    stelina_custom_scrollbar();
-    stelina_countdown();
-    stelina_init_menu_toggle();
-    stelina_custom_scrollbar_header_nav();
+    Fhavy-Scent_custom_scrollbar();
+    Fhavy-Scent_countdown();
+    Fhavy-Scent_init_menu_toggle();
+    Fhavy-Scent_custom_scrollbar_header_nav();
 }); 
--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -36,14 +36,14 @@
         }
     }
 
-    function stelina_resize_megamenu() {
+    function Fhavy-Scent_resize_megamenu() {
         var window_size = jQuery('body').innerWidth();
-        window_size += stelina_get_scrollbar_width();
-        if ( $('.stelina-menu-wapper.horizontal .item-megamenu').length > 0 && window_size > 991 ) {
-            $('.stelina-menu-wapper.horizontal .item-megamenu').each(function () {
+        window_size += Fhavy-Scent_get_scrollbar_width();
+        if ( $('.Fhavy-Scent-menu-wapper.horizontal .item-megamenu').length > 0 && window_size > 991 ) {
+            $('.Fhavy-Scent-menu-wapper.horizontal .item-megamenu').each(function () {
                 var _this            = $(this),
                     _data_responsive = _this.children('.megamenu').data('responsive'),
-                    _container       = _this.closest('.stelina-menu-wapper');
+                    _container       = _this.closest('.Fhavy-Scent-menu-wapper');
                 if ( _data_responsive != '' )
                     _container = _this.closest(_data_responsive);
 
@@ -55,8 +55,8 @@
     /**==============================
      Auto width Vertical menu
      ===============================**/
-    function stelina_auto_width_vertical_menu() {
-        $('.stelina-menu-wapper.vertical.support-mega-menu').each(function () {
+    function Fhavy-Scent_auto_width_vertical_menu() {
+        $('.Fhavy-Scent-menu-wapper.vertical.support-mega-menu').each(function () {
             var menu_offset = $(this).offset(),
                 menu_width  = parseInt($(this).actual('width')),
                 menu_left   = menu_offset.left + menu_width;
@@ -83,7 +83,7 @@
         })
     }
 
-    function stelina_get_scrollbar_width() {
+    function Fhavy-Scent_get_scrollbar_width() {
         var $inner = jQuery('<div style="width: 100%; height:200px;">test</div>'),
             $outer = jQuery('<div style="width:200px;height:150px; position: absolute; top: 0; left: 0; visibility: hidden; overflow:hidden;"></div>').append($inner),
             inner  = $inner[ 0 ],
@@ -99,26 +99,26 @@
     /* ---------------------------------------------
      MOBILE MENU
      --------------------------------------------- */
-    function stelina_menuclone_all_menus() {
-        if ( !$('.stelina-menu-clone-wrap').length && $('.stelina-clone-mobile-menu').length > 0 ) {
-            $('body').prepend('<div class="stelina-menu-clone-wrap">' +
-                '<div class="stelina-menu-panels-actions-wrap">' +
-                '<span class="stelina-menu-current-panel-title">MENU</span>' +
-                '<a class="stelina-menu-close-btn stelina-menu-close-panels" href="#">x</a></div>' +
-                '<div class="stelina-menu-panels"></div>' +
+    function Fhavy-Scent_menuclone_all_menus() {
+        if ( !$('.Fhavy-Scent-menu-clone-wrap').length && $('.Fhavy-Scent-clone-mobile-menu').length > 0 ) {
+            $('body').prepend('<div class="Fhavy-Scent-menu-clone-wrap">' +
+                '<div class="Fhavy-Scent-menu-panels-actions-wrap">' +
+                '<span class="Fhavy-Scent-menu-current-panel-title">MENU</span>' +
+                '<a class="Fhavy-Scent-menu-close-btn Fhavy-Scent-menu-close-panels" href="#">x</a></div>' +
+                '<div class="Fhavy-Scent-menu-panels"></div>' +
                 '</div>');
         }
         var i                = 0,
             panels_html_args = Array();
-        if ( !$('.stelina-menu-clone-wrap .stelina-menu-panels #stelina-menu-panel-main').length ) {
-            $('.stelina-menu-clone-wrap .stelina-menu-panels').append('<div id="stelina-menu-panel-main" class="stelina-menu-panel stelina-menu-panel-main"><ul class="depth-01"></ul></div>');
+        if ( !$('.Fhavy-Scent-menu-clone-wrap .Fhavy-Scent-menu-panels #Fhavy-Scent-menu-panel-main').length ) {
+            $('.Fhavy-Scent-menu-clone-wrap .Fhavy-Scent-menu-panels').append('<div id="Fhavy-Scent-menu-panel-main" class="Fhavy-Scent-menu-panel Fhavy-Scent-menu-panel-main"><ul class="depth-01"></ul></div>');
         }
 
-        $('.stelina-clone-mobile-menu').each(function () {
+        $('.Fhavy-Scent-clone-mobile-menu').each(function () {
             var $this              = $(this),
                 thisOfficeu           = $this,
                 this_menu_id       = thisOfficeu.attr('id'),
-                this_menu_clone_id = 'stelina-menu-clone-' + this_menu_id;
+                this_menu_clone_id = 'Fhavy-Scent-menu-clone-' + this_menu_id;
 
             if ( !$('#' + this_menu_clone_id).length ) {
                 var thisClone = $this.clone(true); // Clone Wrap
@@ -126,143 +126,143 @@
 
                 thisClone.find('[id]').each(function () {
                     // Change all tab links with href = this id
-                    thisClone.find('.vc_tta-panel-heading a[href="#' + $(this).attr('id') + '"]').attr('href', '#' + stelina_menuadd_string_prefix($(this).attr('id'), 'stelina-menu-clone-'));
-                    thisClone.find('.stelina-menu-tabs .tabs-link a[href="#' + $(this).attr('id') + '"]').attr('href', '#' + stelina_menuadd_string_prefix($(this).attr('id'), 'stelina-menu-clone-'));
-                    $(this).attr('id', stelina_menuadd_string_prefix($(this).attr('id'), 'stelina-menu-clone-'));
+                    thisClone.find('.vc_tta-panel-heading a[href="#' + $(this).attr('id') + '"]').attr('href', '#' + Fhavy-Scent_menuadd_string_prefix($(this).attr('id'), 'Fhavy-Scent-menu-clone-'));
+                    thisClone.find('.Fhavy-Scent-menu-tabs .tabs-link a[href="#' + $(this).attr('id') + '"]').attr('href', '#' + Fhavy-Scent_menuadd_string_prefix($(this).attr('id'), 'Fhavy-Scent-menu-clone-'));
+                    $(this).attr('id', Fhavy-Scent_menuadd_string_prefix($(this).attr('id'), 'Fhavy-Scent-menu-clone-'));
                 });
 
-                thisClone.find('.stelina-menu-menu').addClass('stelina-menu-menu-clone');
+                thisClone.find('.Fhavy-Scent-menu-menu').addClass('Fhavy-Scent-menu-menu-clone');
 
                 // Create main panel if not exists
 
-                var thisMainPanel = $('.stelina-menu-clone-wrap .stelina-menu-panels #stelina-menu-panel-main ul');
+                var thisMainPanel = $('.Fhavy-Scent-menu-clone-wrap .Fhavy-Scent-menu-panels #Fhavy-Scent-menu-panel-main ul');
                 thisMainPanel.append(thisClone.html());
 
-                stelina_menu_insert_children_panels_html_by_elem(thisMainPanel, i);
+                Fhavy-Scent_menu_insert_children_panels_html_by_elem(thisMainPanel, i);
             }
         });
     }
 
     // i: For next nav target
-    function stelina_menu_insert_children_panels_html_by_elem($elem, i) {
+    function Fhavy-Scent_menu_insert_children_panels_html_by_elem($elem, i) {
         if ( $elem.find('.menu-item-has-children').length ) {
             $elem.find('.menu-item-has-children').each(function () {
                 var thisChildItem = $(this);
-                stelina_menu_insert_children_panels_html_by_elem(thisChildItem, i);
-                var next_nav_target = 'stelina-menu-panel-' + i;
+                Fhavy-Scent_menu_insert_children_panels_html_by_elem(thisChildItem, i);
+                var next_nav_target = 'Fhavy-Scent-menu-panel-' + i;
 
                 // Make sure there is no duplicate panel id
                 while ( $('#' + next_nav_target).length ) {
                     i++;
-                    next_nav_target = 'stelina-menu-panel-' + i;
+                    next_nav_target = 'Fhavy-Scent-menu-panel-' + i;
                 }
                 // Insert Next Nav
-                thisChildItem.prepend('<a class="stelina-menu-next-panel" href="#' + next_nav_target + '" data-target="#' + next_nav_target + '"></a>');
+                thisChildItem.prepend('<a class="Fhavy-Scent-menu-next-panel" href="#' + next_nav_target + '" data-target="#' + next_nav_target + '"></a>');
 
                 // Get sub menu html
                 var sub_menu_html = $('<div>').append(thisChildItem.find('> .submenu').clone()).html();
                 thisChildItem.find('> .submenu').remove();
 
-                $('.stelina-menu-clone-wrap .stelina-menu-panels').append('<div id="' + next_nav_target + '" class="stelina-menu-panel stelina-menu-sub-panel stelina-menu-hidden">' + sub_menu_html + '</div>');
+                $('.Fhavy-Scent-menu-clone-wrap .Fhavy-Scent-menu-panels').append('<div id="' + next_nav_target + '" class="Fhavy-Scent-menu-panel Fhavy-Scent-menu-sub-panel Fhavy-Scent-menu-hidden">' + sub_menu_html + '</div>');
             });
         }
     }
 
-    function stelina_menuadd_string_prefix(str, prefix) {
+    function Fhavy-Scent_menuadd_string_prefix(str, prefix) {
         return prefix + str;
     }
 
-    function stelina_menuget_url_var(key, url) {
+    function Fhavy-Scent_menuget_url_var(key, url) {
         var result = new RegExp(key + "=([^&]*)", "i").exec(url);
         return result && result[ 1 ] || "";
     }
 
-    function stelina_close_mobile_menu() {
+    function Fhavy-Scent_close_mobile_menu() {
         // BOX MOBILE MENU
         $(document).on('click', '.menu-toggle', function () {
-            $('.stelina-menu-clone-wrap').addClass('open');
+            $('.Fhavy-Scent-menu-clone-wrap').addClass('open');
             return false;
         });
         // Close box menu
-        $(document).on('click', '.stelina-menu-clone-wrap .stelina-menu-close-panels', function () {
-            $('.stelina-menu-clone-wrap').removeClass('open');
+        $(document).on('click', '.Fhavy-Scent-menu-clone-wrap .Fhavy-Scent-menu-close-panels', function () {
+            $('.Fhavy-Scent-menu-clone-wrap').removeClass('open');
             return false;
         });
         $(document).on('click', function (event) {
             if ( $('body').hasClass('rtl') ) {
                 if ( event.offsetX < 0 )
-                    $('.stelina-menu-clone-wrap').removeClass('open');
+                    $('.Fhavy-Scent-menu-clone-wrap').removeClass('open');
             } else {
-                if ( event.offsetX > $('.stelina-menu-clone-wrap').width() )
-                    $('.stelina-menu-clone-wrap').removeClass('open');
+                if ( event.offsetX > $('.Fhavy-Scent-menu-clone-wrap').width() )
+                    $('.Fhavy-Scent-menu-clone-wrap').removeClass('open');
             }
         });
     }
 
     // Open next panel
-    $(document).on('click', '.stelina-menu-next-panel', function (e) {
+    $(document).on('click', '.Fhavy-Scent-menu-next-panel', function (e) {
         var $this     = $(this),
             thisItem  = $this.closest('.menu-item'),
-            thisPanel = $this.closest('.stelina-menu-panel'),
+            thisPanel = $this.closest('.Fhavy-Scent-menu-panel'),
             target_id = $this.attr('href');
 
         if ( $(target_id).length ) {
-            thisPanel.addClass('stelina-menu-sub-opened');
-            $(target_id).addClass('stelina-menu-panel-opened').removeClass('stelina-menu-hidden').attr('data-parent-panel', thisPanel.attr('id'));
+            thisPanel.addClass('Fhavy-Scent-menu-sub-opened');
+            $(target_id).addClass('Fhavy-Scent-menu-panel-opened').removeClass('Fhavy-Scent-menu-hidden').attr('data-parent-panel', thisPanel.attr('id'));
             // Insert current panel title
-            var item_title     = thisItem.find('.stelina-menu-item-title').attr('title'),
+            var item_title     = thisItem.find('.Fhavy-Scent-menu-item-title').attr('title'),
                 firstItemTitle = '';
 
-            if ( $('.stelina-menu-panels-actions-wrap .stelina-menu-current-panel-title').length > 0 ) {
-                firstItemTitle = $('.stelina-menu-panels-actions-wrap .stelina-menu-current-panel-title').html();
+            if ( $('.Fhavy-Scent-menu-panels-actions-wrap .Fhavy-Scent-menu-current-panel-title').length > 0 ) {
+                firstItemTitle = $('.Fhavy-Scent-menu-panels-actions-wrap .Fhavy-Scent-menu-current-panel-title').html();
             }
 
             if ( typeof item_title != 'undefined' && typeof item_title != false ) {
-                if ( !$('.stelina-menu-panels-actions-wrap .stelina-menu-current-panel-title').length ) {
-                    $('.stelina-menu-panels-actions-wrap').prepend('<span class="stelina-menu-current-panel-title"></span>');
+                if ( !$('.Fhavy-Scent-menu-panels-actions-wrap .Fhavy-Scent-menu-current-panel-title').length ) {
+                    $('.Fhavy-Scent-menu-panels-actions-wrap').prepend('<span class="Fhavy-Scent-menu-current-panel-title"></span>');
                 }
-                $('.stelina-menu-panels-actions-wrap .stelina-menu-current-panel-title').html(item_title);
+                $('.Fhavy-Scent-menu-panels-actions-wrap .Fhavy-Scent-menu-current-panel-title').html(item_title);
             }
             else {
-                $('.stelina-menu-panels-actions-wrap .stelina-menu-current-panel-title').remove();
+                $('.Fhavy-Scent-menu-panels-actions-wrap .Fhavy-Scent-menu-current-panel-title').remove();
             }
 
             // Back to previous panel
-            $('.stelina-menu-panels-actions-wrap .stelina-menu-prev-panel').remove();
-            $('.stelina-menu-panels-actions-wrap').prepend('<a data-prenttitle="' + firstItemTitle + '" class="stelina-menu-prev-panel" href="#' + thisPanel.attr('id') + '" data-cur-panel="' + target_id + '" data-target="#' + thisPanel.attr('id') + '"></a>');
+            $('.Fhavy-Scent-menu-panels-actions-wrap .Fhavy-Scent-menu-prev-panel').remove();
+            $('.Fhavy-Scent-menu-panels-actions-wrap').prepend('<a data-prenttitle="' + firstItemTitle + '" class="Fhavy-Scent-menu-prev-panel" href="#' + thisPanel.attr('id') + '" data-cur-panel="' + target_id + '" data-target="#' + thisPanel.attr('id') + '"></a>');
         }
 
         e.preventDefault();
     });
 
     // Go to previous panel
-    $(document).on('click', '.stelina-menu-prev-panel', function (e) {
+    $(document).on('click', '.Fhavy-Scent-menu-prev-panel', function (e) {
         var $this        = $(this),
             cur_panel_id = $this.attr('data-cur-panel'),
             target_id    = $this.attr('href');
 
-        $(cur_panel_id).removeClass('stelina-menu-panel-opened').addClass('stelina-menu-hidden');
-        $(target_id).addClass('stelina-menu-panel-opened').removeClass('stelina-menu-sub-opened');
+        $(cur_panel_id).removeClass('Fhavy-Scent-menu-panel-opened').addClass('Fhavy-Scent-menu-hidden');
+        $(target_id).addClass('Fhavy-Scent-menu-panel-opened').removeClass('Fhavy-Scent-menu-sub-opened');
 
         // Set new back button
         var new_parent_panel_id = $(target_id).attr('data-parent-panel');
         if ( typeof new_parent_panel_id == 'undefined' || typeof new_parent_panel_id == false ) {
-            $('.stelina-menu-panels-actions-wrap .stelina-menu-prev-panel').remove();
-            $('.stelina-menu-panels-actions-wrap .stelina-menu-current-panel-title').html('MAIN MENU');
+            $('.Fhavy-Scent-menu-panels-actions-wrap .Fhavy-Scent-menu-prev-panel').remove();
+            $('.Fhavy-Scent-menu-panels-actions-wrap .Fhavy-Scent-menu-current-panel-title').html('MAIN MENU');
         }
         else {
-            $('.stelina-menu-panels-actions-wrap .stelina-menu-prev-panel').attr('href', '#' + new_parent_panel_id).attr('data-cur-panel', target_id).attr('data-target', '#' + new_parent_panel_id);
+            $('.Fhavy-Scent-menu-panels-actions-wrap .Fhavy-Scent-menu-prev-panel').attr('href', '#' + new_parent_panel_id).attr('data-cur-panel', target_id).attr('data-target', '#' + new_parent_panel_id);
             // Insert new panel title
-            var item_title = $('#' + new_parent_panel_id).find('.stelina-menu-next-panel[data-target="' + target_id + '"]').closest('.menu-item').find('.stelina-menu-item-title').attr('data-title');
+            var item_title = $('#' + new_parent_panel_id).find('.Fhavy-Scent-menu-next-panel[data-target="' + target_id + '"]').closest('.menu-item').find('.Fhavy-Scent-menu-item-title').attr('data-title');
             item_title     = $(this).data('prenttitle');
             if ( typeof item_title != 'undefined' && typeof item_title != false ) {
-                if ( !$('.stelina-menu-panels-actions-wrap .stelina-menu-current-panel-title').length ) {
-                    $('.stelina-menu-panels-actions-wrap').prepend('<span class="stelina-menu-current-panel-title"></span>');
+                if ( !$('.Fhavy-Scent-menu-panels-actions-wrap .Fhavy-Scent-menu-current-panel-title').length ) {
+                    $('.Fhavy-Scent-menu-panels-actions-wrap').prepend('<span class="Fhavy-Scent-menu-current-panel-title"></span>');
                 }
-                $('.stelina-menu-panels-actions-wrap .stelina-menu-current-panel-title').html(item_title);
+                $('.Fhavy-Scent-menu-panels-actions-wrap .Fhavy-Scent-menu-current-panel-title').html(item_title);
             }
             else {
-                $('.stelina-menu-panels-actions-wrap .stelina-menu-current-panel-title').remove();
+                $('.Fhavy-Scent-menu-panels-actions-wrap .Fhavy-Scent-menu-current-panel-title').remove();
             }
         }
 
@@ -274,16 +274,16 @@
      --------------------------------------------- */
 
     $(window).on("resize", function () {
-        stelina_resize_megamenu();
-        stelina_close_mobile_menu();
-        stelina_auto_width_vertical_menu();
+        Fhavy-Scent_resize_megamenu();
+        Fhavy-Scent_close_mobile_menu();
+        Fhavy-Scent_auto_width_vertical_menu();
     });
     window.addEventListener('load',
         function (ev) {
-            stelina_resize_megamenu();
-            stelina_close_mobile_menu();
-            stelina_auto_width_vertical_menu();
-            stelina_menuclone_all_menus();
+            Fhavy-Scent_resize_megamenu();
+            Fhavy-Scent_close_mobile_menu();
+            Fhavy-Scent_auto_width_vertical_menu();
+            Fhavy-Scent_menuclone_all_menus();
         }, false);
 
 })(jQuery); // End of use strict
